b7b46f88ebe5c1a0d196675e2f12e4e6c4853d1f
Lucadmin <luca.account@gmx.de>
2023-02-19 17:30:04 +0100

    Added console secret

--- app/layout.js
+++ app/layout.js
@@ -2,15 +2,18 @@ import "./globals.scss"
 import NavBar from "@/app/components/nav-bar";
 import {Fira_Sans, Montserrat} from "@next/font/google";
 import Footer from "@/app/components/footer";
+import Printer from "@/app/components/printer";
 
 const montserrat = Montserrat({subsets: ['latin'], variable: "--montserrat-font"})
 const firaSans = Fira_Sans({subsets: ['latin'], weight: "400", variable: "--firaSans-font"})
 
 export default function RootLayout({children}) {
+
   return (
       <html lang="en" className={`${montserrat.variable} ${firaSans.variable}`}>
       <head/>
       <body className="font-firaSans bg-xiketic text-white">
+      <Printer/>
       <NavBar/>
       <div className={"min-h-screen"}>
         {children}
