ca38423b250ddb58e2aeca39a62cd08e77e1f0b8
Lucadmin <luca.account@gmx.de>
2024-01-16 19:41:52 +0100

    Built full favicon set

--- app/head.js
+++ app/head.js
@@ -11,7 +11,27 @@ export default function Head() {
         name="description"
         content="Lucadmin eSports offers players the opportunity to join a professional gaming community with top-notch management and support. Discover your potential and reach new heights like immortal or radiant in the world of gaming with Lucadmin eSports."
       />
-      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#060410" />
+      <meta name="msapplication-TileColor" content="#060410" />
+      <meta name="theme-color" content="#ffffff" />
     </>
   );
 }
